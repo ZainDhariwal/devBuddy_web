0fed1c40e709e0d22c90633e67a66719be60771c
Zain Ul Abdin <zainsattar21@gmail.com>
2026-01-01 09:15:07 +0500

    hello world

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,13 @@ function App() {
 
   return (
     <>
-      <h1> Hello world </h1>
+      <h1 className="text-3xl font-bold underline">
+        Hello world!
+      </h1>
     </>
   )
 }
 
 export default App
+
+
