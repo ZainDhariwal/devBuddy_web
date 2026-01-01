0af662b4013cc2a85d79166c9fccbb6946587904
Zain Ul Abdin <zainsattar21@gmail.com>
2026-01-01 11:04:24 +0500

    addded structure of the project

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,24 @@
+import { BrowserRouter, Routes , Route} from "react-router-dom"
+import Navbar from "./Navbar"
+import Body from "./Body"
+import Login from "./Login"
+import Profile from "./Profile"
+
+
 
 function App() {
 
   return (
     <>
-      <h1 className="text-3xl font-bold underline">
-        Hello world!
-      </h1>
+      <BrowserRouter basename="/">
+        <Routes>
+          <Route path="/" element = {<Body />} >
+            <Route path="/login" element = {<Login />} />
+            <Route path="/profile" element = {<Profile />} />
+          
+          </Route>
+        </Routes>
+      </BrowserRouter>
     </>
   )
 }
